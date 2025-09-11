7e50ca9b86323a81e1f1f924f49fca8e675f1ad4
dtyrrelldev <dtyrrell@ualberta.ca>
2025-09-10 20:26:59 -0600

    Add citites, delete city capability
    Added ability to add and delete cities. Still have to work on making it CLEAR that delete city mode is active.

--- ListyCityProj/app/src/main/java/com/example/listycity/MainActivity.java
+++ ListyCityProj/app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.listycity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
     // contains the list of cities
+    boolean deleteMode = false;
+    // whether deletion is available
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +43,41 @@ public class MainActivity extends AppCompatActivity {
         cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
         cityList.setAdapter(cityAdapter);
 
+        // listen for items clicked
+        cityList.setOnItemClickListener((parent, view, position, id) -> {
+            String clickedCity = dataList.get(position);
+
+            if (deleteMode) {   // only delete if you're in "delete city" mode
+                dataList.remove(position);
+                cityAdapter.notifyDataSetChanged();
+                Toast.makeText(this, "Deleted: " + clickedCity, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Clicked: " + clickedCity, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+    public void enableInput(View view){
+        // enables the input box
+        findViewById(R.id.addcity).isEnabled();
+        Log.d("entercity", "enabled");
     }
 
-    public void clearList(View view){
-        dataList.clear();
+    public void addCity(View view){
+        // adds the city to the list
+       TextView text = findViewById(R.id.enterCity);
+        String newCity = text.getText().toString();
+        dataList.add(newCity);
+        cityAdapter.notifyDataSetChanged();  // refreshes the ListView
+
+
     }
+
+    public void deleteCity(View view){
+        // toggles delete mode on click
+        deleteMode = !deleteMode;
+    }
+
+
 }
