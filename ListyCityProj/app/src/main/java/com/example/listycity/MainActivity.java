8d0ffa43bcea8102db4260af3501db8d1eb9eb0a
dtyrrelldev <dtyrrell@ualberta.ca>
2025-09-12 12:48:09 -0600

    Made the input buttons appear and dissapear on add city click

--- ListyCityProj/app/src/main/java/com/example/listycity/MainActivity.java
+++ ListyCityProj/app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.listycity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -60,24 +63,48 @@ public class MainActivity extends AppCompatActivity {
     }
     public void enableInput(View view){
         // enables the input box
-        findViewById(R.id.addcity).isEnabled();
+//        findViewById(R.id.addcity);
         Log.d("entercity", "enabled");
+
+        // make the plainText visible
+        EditText cityInput = findViewById(R.id.enterCity);
+        cityInput.setVisibility(View.VISIBLE);
+
+        // make the confirm button visible
+        findViewById(R.id.addToCityList).setVisibility(View.VISIBLE);
+
+    }
+
+    public void disableInput(View view){
+        // finds and disables the input box + enter button
+        EditText cityInput = findViewById(R.id.enterCity);
+        cityInput.setVisibility(View.INVISIBLE);
+
+        // make the confirm button invisible
+        findViewById(R.id.addToCityList).setVisibility(View.INVISIBLE);
+
     }
 
     public void addCity(View view){
+        // disables the input buttons:
+        disableInput(view);
         // adds the city to the list
        TextView text = findViewById(R.id.enterCity);
         String newCity = text.getText().toString();
         dataList.add(newCity);
         cityAdapter.notifyDataSetChanged();  // refreshes the ListView
-
-
     }
 
     public void deleteCity(View view){
         // toggles delete mode on click
         deleteMode = !deleteMode;
     }
+
+//    public void add_city_layout(View view) {
+//        // open the add_city_layout activity
+//        Intent intent = new Intent(MainActivity.this, add_city_layout.class);
+//        startActivity(intent);
+//    }
 
 
 }
